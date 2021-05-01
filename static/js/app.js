40da15981e783393b22c96c5ed8bf2b61a939b45
petenielsen93 <petenielsen93@gmail.com>
2021-05-01 12:38:48 -0400

    pre-Challenge push for saftey.

--- static/js/app.js
+++ static/js/app.js
@@ -8,15 +8,25 @@ function buildTable(data) {
     tbody.html("");
 }
 //11.5.2 // Loop through each object in data and append a row and cells for each value in row
-data.forEach((dataRow) => {
-    //Append a row to the table body
-    let row = tbody.append("tr");
-    // Loop through each field in dataRow and add each value as a table cell(td)
-    Object.values(dataRow).forEach((val) => {
+function buildTable(data) {
+    // First, clear out any existing data
+    tbody.html("");
+  
+    // Next, loop through each object in the data
+    // and append a row and cells for each value in the row
+    data.forEach((dataRow) => {
+      // Append a row to the table body
+      let row = tbody.append("tr");
+  
+      // Loop through each field in the dataRow and add
+      // each value as a table cell (td)
+      Object.values(dataRow).forEach((val) => {
         let cell = row.append("td");
         cell.text(val);
+        }
+      );
     });
-});
+  }
 
 //11.5.3
 function handleClick() {
